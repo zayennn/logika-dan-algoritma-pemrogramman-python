0a07fe9d370ab5ec9dbc9bf8aa5f512e5daa3702
zayennn <zaayeennn@gmail.com>
2025-09-26 10:54:19 +0700

    first commit

---
+++ logika-dan-algoritma.py
@@ -0,0 +1,11 @@
+angka = 10
+
+if angka < 3 :
+    print('angka kurang dari 3')
+else :
+    print('angka kamu memenuhi persyatatan')
+    
+# cara baca nya :
+# kamu punya angka nilai nya 10
+# terus di kondisiin, jika ( if ) angka ( angka kamu (10) ) nilai nya kurang dari 3 , maka jalanin yang ada di bawah nya
+# kalo angka kamu (10) , nilai nya tidak ngelebihin nilai persyaratan (3) , maka tampiin yang ada dibawah nya
