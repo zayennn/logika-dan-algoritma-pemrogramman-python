dbe6b4dfc8de92ba6e0f985f064a19b6527d172b
zayennn <zaayeennn@gmail.com>
2025-09-26 10:56:58 +0700

    update

--- logika-dan-algoritma.py
+++ logika-dan-algoritma.py
@@ -1,3 +1,4 @@
+# pengkondisian / if else
 angka = 10
 
 if angka < 3 :
@@ -9,3 +10,12 @@ else :
 # kamu punya angka nilai nya 10
 # terus di kondisiin, jika ( if ) angka ( angka kamu (10) ) nilai nya kurang dari 3 , maka jalanin yang ada di bawah nya
 # kalo angka kamu (10) , nilai nya tidak ngelebihin nilai persyaratan (3) , maka tampiin yang ada dibawah nya
+
+
+
+
+# perulangan / for
+
+for i in range(5) :
+    print(f'ini perulangan ke-${i}')
+    
